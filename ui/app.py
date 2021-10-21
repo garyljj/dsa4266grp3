1b55e8194062393d900d5d2bf2ce2a5bcda8c2ed
colinngcy2 <70565553+colinngcy2@users.noreply.github.com>
2021-10-21 14:28:48 +0800

    80% ready, waiting for api
    - fixed bug on non-image files upload
    - can now upload an image and will be saved into the uploaded_image folder in ui/static/uploaded_image

--- ui/app.py
+++ ui/app.py
@@ -8,6 +8,7 @@ import pandas as pd
 import sys
 
 UPLOAD_FOLDER = './static/uploaded_image'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
@@ -15,20 +16,25 @@ app.config['SECRET_KEY'] = 'pokemon'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 Bootstrap(app)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = UploadImageForm()
     if request.method == 'POST':
         data_file = request.files['data_file']
-        filename = data_file.filename
-        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        data_file.save(path)
-        session["filename"] = filename
-        if "control_id" not in session:
-            session["control_id"] = 1
-        else:
-            session["control_id"] += 1
-        return redirect(url_for('result_download'))
+        if data_file and allowed_file(data_file.filename):
+            filename = data_file.filename
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            data_file.save(path)
+            session["filename"] = filename
+            if "control_id" not in session:
+                session["control_id"] = 1
+            else:
+                session["control_id"] += 1
+            return redirect(url_for('result_download'))
     '''
     if form.validate_on_submit():
         file = form.data_file.data
