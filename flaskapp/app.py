87388db34827804ba6abbf4d941e2e84defdbac2
garyljj <gary.lee@u.nus.edu>
2021-10-27 05:52:56 +0800

    Integrate app

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -6,6 +6,10 @@ from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
 import pandas as pd
 import sys
+import base64
+import random
+import json
+
 
 UPLOAD_FOLDER = './static/uploaded_image'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -51,3 +55,31 @@ def result_download():
     filename = session.get("filename", None)
     control_id = session.get("control_id", None)
     return render_template('result_download.html', filename=filename, control_id=control_id)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = json.loads(request.get_json())
+    img = base64.b64decode(data['image_base64'].encode('utf8'))
+    data['prediction'] = get_prediction(img)
+
+    return json.dumps(data)
+
+def get_prediction(img):
+    """
+    TODO THIS IS WHERE OUR ENTIRE PREDICTION CODE WILL GO
+    """
+
+    preds = [mock_pred() for i in range(3)]
+    return preds
+
+def mock_pred():
+    return {
+        'predicted_class': random.randint(1,4),
+        'confidence': random.random(),
+        'bounding_box': [random.random(), random.random(), random.random(), random.random()]
+    }
+
+@app.route('/testpage')
+def test():
+    return 'this is a testpage'
