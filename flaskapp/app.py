9c8340c0502bf480df3f8d7324987ddcdb09305d
colinngcy2 <70565553+colinngcy2@users.noreply.github.com>
2021-11-02 01:16:17 +0800

    Simple fix on upload

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,22 +1,20 @@
 import os
-from flask import Flask, render_template, flash, redirect, url_for, session, request, flash
-from .forms import UploadImageForm, PreviewImageForm
-import pandas as pd
-import sys
 import base64
 import random
 import json
 import time
-from datetime import datetime
-from .model import mask_img
 import numpy as np
 import cv2
+from flask import Flask, render_template, redirect, url_for, session, request, flash
+from .forms import UploadImageForm, PreviewImageForm
+from datetime import datetime
+from .model import mask_img
+from flask_bootstrap import Bootstrap
 
-UPLOAD_FOLDER = './static/uploaded_image'
-PREVIEW_FOLDER = './static/preview'
+UPLOAD_FOLDER = 'flaskapp/static/uploaded_image'
+PREVIEW_FOLDER = '../static/preview'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pokemon'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -29,27 +27,20 @@ def allowed_file(filename): #returns True if is an image
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    print("HERE")
     form2 = PreviewImageForm()
     form1 = UploadImageForm()
 
     if form2.validate_on_submit():
-        print("HERE2")
         image = request.files['data_file_preview']
         image_name = image.filename
-
         npimg = np.fromfile(image, np.uint8)
         image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
         masked_image = mask_img(image)
         cv2.imwrite(os.path.join(app.config['PREVIEW_FOLDER'], image_name), masked_image)
-
         return render_template('home.html', form1=form1, form2=form2, image=masked_image, image_name=image_name)
 
     if request.method == 'POST':
-        # For more than one file upload
         all_files = request.files.getlist("data_file")
-        print(all_files)
-
         if not all(allowed_file(x.filename) == True for x in all_files):
             flash('Upload failed! Please ensure to only upload an image file.', 'alert-danger')
             return redirect(url_for('home') +"#contact")
@@ -57,18 +48,16 @@ def home():
         if all(allowed_file(x.filename) == True for x in all_files):
             unique_num = str(str(datetime.today()).split(".")[0])[-9:].replace(':', '')
             session['unique_num'] = unique_num
-            print(unique_num)
+            print("Unique Number: ", unique_num)
             for file in all_files:
                 filename = file.filename
                 path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 file.save(path)
 
             mask = form1.mask.data  ## mask refers to the boolean of either true or false
-            print(mask)
-            #results = run_predictions(all_files, mask)
-            results = run_predictions(all_files)
+            print("Masking =", mask)
+            results = run_predictions(all_files, mask)
             print(results)
-            #return render_template('result_download.html', unique_num=unique_num)
             return redirect(url_for('result_download'))
     return render_template('home.html', form1=form1, form2=form2)
 
--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,16 +1,10 @@
-from flask_wtf import FlaskForm, Form
-from wtforms import *
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField, IntegerField, FileField, RadioField, FormField, FieldList, MultipleFileField, DecimalField
-from wtforms.validators import ValidationError, DataRequired, EqualTo, Length
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, SubmitField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from werkzeug.utils import secure_filename
-#from app.models import User
-#from datetime import datetime,date,timedelta
 
 class UploadImageForm(FlaskForm):
     data_file = FileField('Upload Image File', render_kw={'multiple': True}, validators=[FileRequired(), FileAllowed(['png', 'jpeg', 'jpg'], 'Image Files only!')])
     mask = BooleanField('Mask', default=True)
-    #mask = SelectField(u'Mask', choices=[('Yes', 'No')])
     submit = SubmitField('Upload')
 
 class PreviewImageForm(FlaskForm):
