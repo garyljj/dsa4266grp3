7a49b64664e18b4a138f5af3c0f882b56d1997b0
colinngcy2 <70565553+colinngcy2@users.noreply.github.com>
2021-11-09 17:04:51 +0800

    clean up push

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -5,7 +5,7 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 class UploadImageForm(FlaskForm):
     data_file = FileField('Upload Image File', render_kw={'multiple': True}, validators=[FileRequired(), FileAllowed(['jpg'], 'Image Files only!')])
     mask = BooleanField('Mask (Tick to enable masking for prediction)', default=True)
-    fast = BooleanField('Fast Prediction (Tick to enable faster weights. Smaller model which is up to 4x faster!', default=True)
+    fast = BooleanField('Fast Prediction (Tick to enable faster weights. Smaller model which is up to 4x faster!)', default=True)
     submit = SubmitField('Upload')
 
 class PreviewImageForm(FlaskForm):
